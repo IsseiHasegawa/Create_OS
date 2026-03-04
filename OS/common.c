944076c66a62ee59566606db9ed0654c73ac3dfc
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-03-04 11:33:16 -0500

    Add functions in common.c

--- OS/common.c
+++ OS/common.c
@@ -59,3 +59,37 @@ void printf(const char *fmt, ...) {
 end:
     va_end(vargs);
 }
+
+void *memcpy(void *dst, const void *src, size_t n) {
+    unit8_t *d = (unit8_t *) dst;
+    const unit8_t *s = (const unit8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+    unit8_t *p = (unit8_t *) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+char *strcpy(char *dst, const *src) {
+    char *d = dst;
+    while (*src)
+        *d++ = *src++;
+    *d = '\0';
+    return dst;
+}
+
+char strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2) {
+        if (*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
