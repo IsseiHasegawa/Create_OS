cab8447dc119f50b2b572df27ff9b0e3a1a22416
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-03-04 11:46:43 -0500

    Fix erorr

--- OS/common.c
+++ OS/common.c
@@ -61,21 +61,21 @@ end:
 }
 
 void *memcpy(void *dst, const void *src, size_t n) {
-    unit8_t *d = (unit8_t *) dst;
-    const unit8_t *s = (const unit8_t *) src;
+    uint8_t *d = (uint8_t *) dst;
+    const uint8_t *s = (const uint8_t *) src;
     while (n--)
         *d++ = *s++;
     return dst;
 }
 
 void *memset(void *buf, char c, size_t n) {
-    unit8_t *p = (unit8_t *) buf;
+    uint8_t *p = (uint8_t *) buf;
     while (n--)
         *p++ = c;
     return buf;
 }
 
-char *strcpy(char *dst, const *src) {
+char *strcpy(char *dst, const char *src) {
     char *d = dst;
     while (*src)
         *d++ = *src++;
@@ -83,7 +83,7 @@ char *strcpy(char *dst, const *src) {
     return dst;
 }
 
-char strcmp(const char *s1, const char *s2) {
+int strcmp(const char *s1, const char *s2) {
     while (*s1 && *s2) {
         if (*s1 != *s2)
             break;
